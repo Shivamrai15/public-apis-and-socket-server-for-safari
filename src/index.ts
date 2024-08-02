9d9c047b7dfa1acaaa02c8257ba17220946ca3d8
Shivam <shivamrai6836@gmail.com>
2024-08-02 20:46:42 +0530

    update cors origin

--- src/index.ts
+++ src/index.ts
@@ -19,14 +19,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors : {
-        origin : "http://localhost:3000",
+        origin : process.env.CLIENT || "http://localhost:3000",
         methods : ["GET", "POST"]
     }
 });
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT || "http://localhost:3000",
     methods : ["GET", "POST"]
 }));
 
